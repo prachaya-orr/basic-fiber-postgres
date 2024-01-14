e8f016a97fe2ed3127cda9fb76a569dbcbfaf76a
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2024-01-14 23:28:27 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/prachaya-or/fiber-postgres
+
+go 1.21.1
+
+require github.com/lib/pq v1.10.9
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host         = "localhost"
+	port         = 5432
+	databaseName = "mydatabase"
+	username     = "myuser"
+	password     = "mypassword"
+)
+
+var db *sql.DB
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+}
+
+func main() {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, username, password, databaseName)
+
+	sdb, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = sdb
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Connection database successful
+	fmt.Println("Connection database successful")
+
+	// err = createProduct(&Product{Name: "Product 2", Price: 2000})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// product, err := getProduct(2)
+	// fmt.Println("Get Successful !", product)
+
+	// err = updateProduct(4, &Product{Name: "XYZ", Price: 300})
+	product, err := updateProduct(4, &Product{Name: "UUU", Price: 300})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Update Successful !", product)
+
+	err = deleteProduct(4)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Delete product Successful !")
+}
+
+func createProduct(product *Product) error {
+
+	_, err := db.Exec(
+		"INSERT INTO public.products (name, price) VALUES ($1, $2);", product.Name, product.Price)
+
+	return err
+}
+
+func getProduct(id int) (Product, error) {
+	var p Product
+	row := db.QueryRow(
+		"SELECT id,name,price FROM products WHERE id = $1", id)
+	err := row.Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		fmt.Println("Error Scan", err)
+		return Product{}, err
+	}
+
+	return p, nil
+}
+
+func updateProduct(id int, product *Product) (Product, error) {
+	// _, err := db.Exec(
+	// 	"UPDATE public.products	SET name=$1, price =$2 WHERE id = $3;",
+	// 	product.Name,
+	// 	product.Price,
+	// 	id)
+
+	// return err
+
+	var p Product
+	row := db.QueryRow("UPDATE public.products	SET name=$1, price =$2 WHERE id = $3 RETURNING id,name,price;", product.Name, product.Price, id)
+
+	err := row.Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		fmt.Println("Error Scan", err)
+		return Product{}, err
+	}
+
+	return p, err
+}
+
+func deleteProduct(id int) error {
+	_, err := db.Exec(
+		"DELETE FROM products WHERE id = $1;", id,
+	)
+
+	return err
+}
